9f2b7b7fef26987171cdd9672709b50d8207b9bf
Vika <vikusagerman@gmail.com>
2021-04-02 16:09:55 +0300

    Refactor controllers and routes in base skeleton

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
-const { getProducts } = require("../controllers/productController")
+const { getHomePage, getProducts } = require("../controllers/productController")
 
-router.route('/products').get(getProducts)
+router.route("/main").get(getHomePage);
+router.route("/products").get(getProducts)
 
 module.exports = router;

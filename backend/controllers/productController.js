dc9235071db324710f73316bdc78a2965852e664
Mike Pereverziy <nagato9423@gmail.com>
2021-04-04 13:21:24 +0300

    page404

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,3 +4,10 @@ exports.getProducts = (req, res, next) => {
         message: "getProducts"
     })
 }
+
+exports.get404Page = (req, res, next) => {
+    res.status(404).json({
+        success: true,
+        message: "Page-404"
+    })    
+}
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
 const router = express.Router();
-const { getProducts } = require("../controllers/productController")
+const { getProducts } = require("../controllers/productController");
+const { get404Page } = require("../controllers/productController");
 
-router.route('/products').get(getProducts)
+
+router.route('/products').get(getProducts);
+router.route('*').get(get404Page);
+
 
 module.exports = router;

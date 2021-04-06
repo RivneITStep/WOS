c628a3bb0e059cb049c38ef738d2c8a981169371
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2021-04-06 19:39:24 +0300

    Added react-bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react"
+// Import components 
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+
+  );
+}
+
+export default App;

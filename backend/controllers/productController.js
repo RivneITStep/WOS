9b98c613ed1a0cfb6a2dafbdde91805a54aa17e0
Mike Pereverziy <nagato9423@gmail.com>
2021-04-07 21:43:19 +0300

    add concurrently

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,8 +9,9 @@ exports.get404Page = (req, res, next) => {
     res.status(404).json({
         success: true,
         message: "Page-404"
-    })    
-  
+    }) 
+}
+
 exports.getHomePage = (req, res, next) => {
     res.status(200).json({
         success: true,

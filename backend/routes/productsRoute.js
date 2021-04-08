40f8d3c47fe7a26eb9137bb170c495f04cc433a6
Vika <vikusagerman@gmail.com>
2021-04-08 11:18:01 +0300

    Conection to mongoose

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage, getProducts, get404Page } = require("../controllers/productController")
+const { getHomePage, getProducts, get404Page } = require("../controllers/productController");
 
 router.route("/main").get(getHomePage);
 router.route("/products").get(getProducts)

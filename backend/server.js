02a5482df05a737eae5d7aa625224c5662a4be53
Maria Savchuk <savchuk_maria@ukr.net>
2021-04-13 16:39:06 +0300

    Added connection to MongoDB and Footer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
+const mongoose = require("mongoose");
+
+const MONGODB_URI = 'mongodb+srv://Masha:2011qW@wos.lbu2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI || "mongodb://localhost/WOS", {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected');
+});
+
 dotenv.config({ path: 'backend/config/config.env' });
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`))
--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -4,22 +4,41 @@ import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button } from "
 const Header = () => {
     return (
         <header>
-            <Navbar bg="light" className="navbar navbar-expand-lg navbar-dark bg-primary" expand="lg">
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <Navbar.Brand className="navbar-brand">WOS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link className="nav-link">Home</Nav.Link>
+                    <Nav.Link className="nav-link">Profile </Nav.Link>
+                    <Nav.Link className="nav-link">Sign In</Nav.Link>
+                    <Nav.Link className="nav-link">Cart </Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+            {/* <Navbar bg="light" className="navbar navbar-expand-lg navbar-dark bg-primary" expand="lg">
                 <Navbar.Brand className="navbar-brand" href="#home">WOShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto navbar-nav">
-                        <Nav.Link className="nav-link" href="#home">Home</Nav.Link>
-                        <Nav.Link className="nav-link" href="#profile">Profile</Nav.Link>
-                        <Nav.Link className="nav-link" href="#cart">Cart</Nav.Link>
-                        <Nav.Link className="nav-link" href="#signIn">Sign In</Nav.Link>
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="form-control mr-sm-2" />
                         <Button className="btn btn-secondary my-2 my-sm-0" >Search</Button>
                     </Form>
                 </Navbar.Collapse>
-            </Navbar>
+            </Navbar> */}
         </header>
     )
 }

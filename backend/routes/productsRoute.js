d98f7d29e082e8535290acde01d5690fd28c1b18
Iryna <65454947+iraproza@users.noreply.github.com>
2021-04-15 21:34:45 +0300

    Revert "Devops"

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage, getProducts, get404Page, getSingleProduct, postDeleteProduct } = require("../controllers/productController");
+const { getHomePage, getProducts, get404Page } = require("../controllers/productController");
 
 router.route("/main").get(getHomePage);
-router.route("/products").get(getProducts);
-router.route("/products/:id").get(getSingleProduct);
-router.route("/products-delete:id").get(postDeleteProduct);
+router.route("/products").get(getProducts)
 router.route('*').get(get404Page);
 
 module.exports = router;

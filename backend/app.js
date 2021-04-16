5712c703751b90bd9d9eeb121c51fabb592f5a75
Maria Savchuk <savchuk_maria@ukr.net>
2021-04-16 22:12:00 +0300

    Get all products and get single product by id

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 const productRouter = require("./routes/productsRoute")
 
-app.use("/api/v1/", productRouter)
+app.use("/api/v1/",productRouter)
 
 module.exports = app;

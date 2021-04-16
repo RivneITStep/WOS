1eee09eb5ce0f4bcd926d715079891775dceba15
Iraproza <kolomiira@gmail.com>
2021-04-16 19:15:55 +0300

    add delete product by id

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage, getProducts, get404Page } = require("../controllers/productController");
+const { getHomePage, getProducts, get404Page, deleteProduct } = require("../controllers/productController");
 
 router.route("/main").get(getHomePage);
-router.route("/products").get(getProducts)
+router.route("/products").get(getProducts);
+router.route("/products-delete:id").get(deleteProduct);
 router.route('*').get(get404Page);
 
 module.exports = router;

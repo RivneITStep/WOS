89d68a153af0e8ed4b68256a2c8dceb534460e3f
Vika <vikusagerman@gmail.com>
2021-04-17 16:16:58 +0300

    Add new product

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage, getProducts, get404Page, getSingleProduct } = require("../controllers/productController");
+const { getHomePage, getProducts, get404Page,addProduct } = require("../controllers/productController");
 
 router.route("/main").get(getHomePage);
 router.route("/products").get(getProducts)
-router.route("/product/:id").get(getSingleProduct)
+router.route("/products-add:id").get(addProduct)
 router.route('*').get(get404Page);
 
 module.exports = router;

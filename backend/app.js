5c0bf0eca2205e36c2cabba1d214ab3f3e508795
Maria Savchuk <savchuk_maria@ukr.net>
2021-04-21 21:04:23 +0300

    Added errorHandler, fixed productController and changed type of connection in productsRoute

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
+//Error Middleware
+const errorMiddleware = require("./middleware/errors");
+
 const app = express();
 const productRouter = require("./routes/productsRoute")
 
 app.use("/api/v1/",productRouter)
+
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHomePage, getProducts, get404Page, getSingleProduct, deleteProduct, addProduct, editProduct } = require("../controllers/productController");
+const productController = require("../controllers/productController");
 
-router.route("/main").get(getHomePage);
-router.route("/products").get(getProducts);
-router.route("/products-delete:id").get(deleteProduct);
-router.route("/product/:id").get(getSingleProduct)
-router.route("/products-add:id").get(addProduct)
-router.route("/product-edit/:id").get(editProduct);
-router.route('*').get(get404Page);
+router.get("/main", productController.getHomePage);
+router.get("/products", productController.getProducts);
+router.get("/product/:id", productController.getSingleProduct);
+router.delete("/products-delete/:id", productController.deleteProduct);
+router.post("/products-add/:id", productController.addProduct);
+router.put("/product-edit/:id", productController.editProduct);
+router.get("/*", productController.get404Page);
+
 
 module.exports = router;
